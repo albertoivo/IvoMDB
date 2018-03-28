c39ce246f456632ed028b7502576b829195491c0
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-03-28 14:54:09 -0300

    inheritance

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,9 @@
-import movie
+from movie import Movie
 import fresh_tomatoes
 
-toy_story = movie.Movie("Toy Story", "A stoyry of a boy and his toys that come to life",
-                        "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg", "https://www.youtube.com/watch?v=4KPTXpQehio")
+toy_story = Movie("Toy Story", "A story of a boy and his toys that come to life",
+                  "https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-m8zzyx_5670999f.jpeg",
+                  "https://www.youtube.com/watch?v=4KPTXpQehio")
 
 movies = [toy_story]
 fresh_tomatoes.open_movies_page(movies)
