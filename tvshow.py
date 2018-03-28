f4fcbe4e0d83b7bbe015aa6bf194d24a008ca92c
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-03-28 16:44:24 -0300

    creating a Video child

--- tvshow.py
+++ tvshow.py
@@ -1,12 +1,7 @@
 import webbrowser
+from video import Video
 
 
-class Movie():
+class TVShow(Video):
     def __init__(self, title, storyline, poster, trailer):
-        self.title = title
-        self.storyline = storyline
-        self.poster = poster
-        self.trailer = trailer
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer)
+        Video.__init__(self, title, storyline, poster, trailer)
