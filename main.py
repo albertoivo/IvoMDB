cff721d6527733609ba8595bc542a0498d68f730
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-03-30 08:46:57 -0300

    initial msg

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import tmdbsimple as tmdb
 tmdb.API_KEY = 'fb63a0c036ee1bc7d7a9f95390b63d32'
 
 print('Loading... Please wait a moment...')
+print('Searching for random movies in the The Movie Database API.')
+print('This may take a while.')
 
 movies = movieDB.getMovies()
 shows = showDB.getTVShows()
